b376c1bed305cffe1a10ed805ad0dacfa4939d5c
prkpwm <prkpwm.dev@gmail.com>
2023-07-23 15:46:24 +0700

    code

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,40 @@
 #include <Arduino.h>
-// borad: Arduino Uno
 
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int relayPin = 7; // the number of the relay pin
-// built-in LED
-const int ledPin = LED_BUILTIN; // the number of the LED pin
+const int RELAY_PIN = 7;
+const int SOIL_MOISTURE_PIN = 6;
+const int WORKING_TIME_THRESHOLD = 60000;
 
+unsigned long workingStartTime = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, HIGH);
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
-  Serial.println("Hello World");
- 
+// Function declarations
+void relayHandler(int relayPin, int relayState);
+
+void setup()
+{
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(SOIL_MOISTURE_PIN, INPUT);
+  Serial.begin(115200);
+  relayHandler(RELAY_PIN, HIGH);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int sensorValue = analogRead(analogInPin);
-  Serial.println(sensorValue);
-  if (sensorValue > 500) {
-    digitalWrite(relayPin, LOW);
-    digitalWrite(ledPin, HIGH);
+void loop()
+{
 
-  } else {
-    digitalWrite(relayPin, HIGH);
-    digitalWrite(ledPin, LOW);
+  int soilMoisture = digitalRead(SOIL_MOISTURE_PIN);
+
+  if (soilMoisture == LOW)
+  {
+    relayHandler(RELAY_PIN, LOW);
+    workingStartTime = millis();
   }
-  delay(1000);
+  else if (millis() - workingStartTime < WORKING_TIME_THRESHOLD)
+  {
+    // Do nothing while waiting
+  }
+  else
+  {
+    relayHandler(RELAY_PIN, HIGH);
+  }
+
+  delay(200);
 }
-a
